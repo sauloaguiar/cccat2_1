c754b49e8e239d541363eaca27b29af54c9d363d
Saulo <sauloaguiar89@gmail.com>
2021-08-13 16:28:56 -0300

    adds static freightCaclulator helper

---
+++ src/FreightCalculator.test.ts
@@ -0,0 +1,23 @@
+import FreightCalculator from './FreightCalculator';
+import Item from './Item';
+
+test('Deve custar R$ 30,00 para enviar uma guitarra', () => {
+  const guitarra = new Item("1", "Guitarra", 1000, 100, 50, 15, 3);
+  const distance = 1000;
+  const cost = FreightCalculator.calculate(distance, guitarra);
+  expect(cost).toBe(30);
+});
+
+test('Deve custar R$ 220,00 para enviar um aplificador', () => {
+  const item = new Item("2", "Amplificador", 5000, 50, 50, 50, 22);
+  const distance = 1000;
+  const cost = FreightCalculator.calculate(distance, item);
+  expect(cost).toBe(220);
+});
+
+test('Deve custar R$ 10,00 para enviar um cabo', () => {
+  const item = new Item("3", "Cabo", 30, 10, 10, 10, 1)
+  const distance = 1000;
+  const cost = FreightCalculator.calculate(distance, item);
+  expect(cost).toBe(10);
+});
